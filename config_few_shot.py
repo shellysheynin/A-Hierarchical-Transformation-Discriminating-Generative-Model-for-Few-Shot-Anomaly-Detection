067313a89d85bd5a40b390cd85275dc6d0805feb
shellysheynin <42910792+shellysheynin@users.noreply.github.com>
2021-04-25 13:18:43 +0300

    Add files via upload

---
+++ config_few_shot.py
@@ -0,0 +1,43 @@
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    # parser.add_argument('--mode', help='task to be done', default='train')
+    # workspace:
+    parser.add_argument('--not_cuda', action='store_true', help='disables cuda', default=0)
+
+    # load, input, save configurations:
+    parser.add_argument('--netG', default='', help="path to netG (to continue training)")
+    parser.add_argument('--netD', default='', help="path to netD (to continue training)")
+    parser.add_argument('--manualSeed', type=int, help='manual seed')
+    parser.add_argument('--nc_z', type=int, help='noise # channels', default=3)
+    parser.add_argument('--nc_im', type=int, help='image # channels', default=3)
+    parser.add_argument('--out', help='output folder', default='Output')
+
+    # networks hyper parameters:
+    parser.add_argument('--nfc', type=int, default=32)
+    parser.add_argument('--min_nfc', type=int, default=32)
+    parser.add_argument('--ker_size', type=int, help='kernel size', default=3)
+    parser.add_argument('--num_layer', type=int, help='number of layers', default=5)
+    parser.add_argument('--stride', help='stride', default=1)
+    parser.add_argument('--padd_size', type=int, help='net pad size', default=0)  # math.floor(opt.ker_size/2)
+
+    # pyramid parameters:
+    parser.add_argument('--scale_factor', type=float, help='pyramid scale factor', default=0.75)  # pow(0.5,1/6))
+    parser.add_argument('--noise_amp', type=float, help='addative noise cont weight', default=0.1)
+    parser.add_argument('--min_size', type=int, help='image minimal size at the coarser scale', default=25)
+    parser.add_argument('--max_size', type=int, help='image minimal size at the coarser scale', default=64)
+
+    # optimization hyper parameters:
+    parser.add_argument('--niter', type=int, default=5000, help='number of iterations to train per scale')
+    parser.add_argument('--gamma', type=float, help='scheduler gamma', default=0.1)
+    parser.add_argument('--lr_g', type=float, default=0.0005, help='learning rate, default=0.0005')
+    parser.add_argument('--lr_d', type=float, default=0.0005, help='learning rate, default=0.0005')
+    parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
+    parser.add_argument('--Gsteps', type=int, help='Generator inner steps', default=3)
+    parser.add_argument('--Dsteps', type=int, help='Discriminator inner steps', default=3)
+    parser.add_argument('--lambda_grad', type=float, help='gradient penelty weight', default=0.1)
+    parser.add_argument('--alpha', type=float, help='reconstruction loss weight', default=100)
+
+    return parser
---
+++ main_train.py
@@ -0,0 +1,82 @@
+from FewShot_models.training_parallel import *
+import FewShot_models.functions as functions
+import numpy as np
+import torch.utils.data
+from Dataloaders.cifar_loader import download_class_cifar
+from Dataloaders.mnist_loader import download_class_mnist
+from Dataloaders.fashionmnist_loader import download_class_FashionMnist
+from Dataloaders.paris_loader import download_class_paris
+from Dataloaders.mvtec_loader import download_class_mvtec
+from anomaly_detection_evaluation import anomaly_detection
+from defect_detection_evaluation import defect_detection
+
+if __name__ == '__main__':
+    parser = get_arguments()
+    parser.add_argument('--dataset', help='cifar/mnist/fashionmnist/mvtec/paris', default='cifar')
+    parser.add_argument('--input_dir', help='input image dir', default='Input/Images')
+    parser.add_argument('--pos_class', help='normal class', required=True)
+    parser.add_argument('--random_images_download', help='random selection of images', default=False)
+    parser.add_argument('--num_images', type=int, help='number of images to train on', default=1)
+    parser.add_argument('--if_download_class', type=bool, help='do you want to download class', default=True)
+    parser.add_argument('--mode', help='task to be done', default='train')
+    parser.add_argument('--size_image', type=int, help='size orig image', default=128)
+    parser.add_argument('--num_epochs', type=int, help='num epochs', default=1)
+    parser.add_argument('--policy', default='')
+    parser.add_argument('--index_download', help='index in dataset for starting download', type=int, default=1)
+    parser.add_argument('--use_internal_load', help='using another dataset', default=False)
+    parser.add_argument('--experiment', help='task to be done', default='stop_signs')
+    parser.add_argument('--test_size', help='test size', type=int, default=10000)
+    parser.add_argument('--num_transforms', help='54 for rgb, 42 for grayscale', type=int, default=54)
+    parser.add_argument('--device_ids', help='gpus ids in format: 0/ 0 1/ 0 1 2..', nargs='+', type=int, default=0)
+    parser.add_argument('--fraction_defect', help='fraction of patches to consider in each scale', nargs='+', type=float, default=0.1)
+
+
+    opt = parser.parse_args()
+    scale = opt.size_image
+    pos_class = opt.pos_class
+    random_images = opt.random_images_download
+    num_images = opt.num_images
+    opt.num_transforms=opt.num_transforms
+    dataset = opt.dataset
+
+    if opt.if_download_class == True:
+        if dataset == 'cifar':
+            opt.num_transforms = 54
+            opt.input_name = download_class_cifar(opt)
+        elif dataset == 'mnist':
+            opt.num_transforms = 42
+            opt.input_name = download_class_mnist(opt)
+        elif dataset == 'fashionmnist':
+            opt.num_transforms = 42
+            opt.input_name = download_class_FashionMnist(opt)
+        elif dataset == 'paris':
+            opt.num_transforms = 54
+            opt.input_name = download_class_paris(opt)
+        elif dataset == 'mvtec':
+            opt.num_transforms = 54
+            opt.input_name = download_class_mvtec(opt)
+
+
+    opt = functions.post_config(opt)
+    Gs = []
+    Zs,NoiseAmp = {}, {}
+    reals_list = torch.FloatTensor(num_images,1,int(opt.nc_im), int(opt.size_image), int(opt.size_image)).cuda()
+
+    for i in range(num_images):
+        real = img.imread("%s/%s_%d.png" % (opt.input_dir, opt.input_name[:-4], i))
+        if dataset == 'mnist' or dataset == 'fashionmnist':
+            real = torch.from_numpy(real).unsqueeze(2)
+            real = real.repeat(1, 1, 3).numpy()
+        real = functions.np2torch(real, opt)
+        real = real[:, 0:3, :, :]
+        functions.adjust_scales2image(real, opt)
+    dir2save = functions.generate_dir2save(opt)
+    reals = {}
+    lst =  np.load("TrainedModels/" + str(opt.input_name)[:-4] +  "/transformations.npy")
+    opt.list_transformations = lst
+    if opt.mode == 'train':
+        train(opt, Gs, Zs, reals, NoiseAmp)
+    if dataset == 'mvtec':
+        defect_detection(opt.input_name, opt.test_size, opt)
+    else:
+        anomaly_detection(opt.input_name, opt.test_size, opt)
